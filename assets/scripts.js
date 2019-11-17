7b419ec4d8b4495a3659c6eaf7bdcacd229c060f
Chris Simpson <chris_simpson@live.com.au>
2019-11-17 16:44:58 +0000

    project complete

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,8 +2,9 @@ $( document ).ready(function() {
     $('.button').mousedown(function() {
         $(this).find('audio.air-horn')[0].play();
     });
-    $( ".button" ).mouseup( function() {
-        var audio = $(this).find( "audio.air-horn" )[0];
+    
+    $('.button').mouseup(function() {
+        var audio = $(this).find('audio.air-horn')[0];
         audio.pause();
         audio.currentTime = 0;
     });
